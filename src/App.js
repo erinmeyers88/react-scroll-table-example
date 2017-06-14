f5c58c01fd3ed741cfefa0f939a8bcb630951fad
Vincent, Erin <evincent@ariit.com>
2017-06-14 14:52:10 -0600

    updating with icons passed in

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ const tableProps = {
     {favorite: false, username: 'Han Solo', text: 'Laugh it up, fuzzball!', date: new Date(1990, 4, 7).toDateString()},
     {favorite: true, username: 'Yoda', text: 'Try not. Do—or do not. There is no try.', date: new Date(2005, 6, 12).toDateString()},
     {favorite: false, username: 'Leia Organa', text: 'Help me Obi-Wan Kenobi, you\'re my only hope.', date: new Date(2012, 1, 27).toDateString()}],
-  downIcon: <i className="fa fa-down"/>
+  downIcon: <i className="fa fa-down"/>,
   maxHeight: 150,
   noDataText: 'no data here',
   shaded: true,
